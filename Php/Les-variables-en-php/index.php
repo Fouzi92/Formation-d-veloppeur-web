0e75d1a5fa95028f935db442ad1daec45fbcf25a
Fouzi92 <fouzi457@hotmail.fr>
2023-06-13 23:40:04 +0200

    Formation developpeur

---
+++ Php/Les-variables-en-php/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+       $maVariable1 = 10;
+       $maVariable2 = 25;
+       $result = $maVariable1 + $maVariable2;
+
+       echo $result ;
+
+       $maVariable3 = "Salut , je suis un pessi";
+       $maVariable4 = "On apprend le php";
+       $version = 7;
+
+       echo "php Versopn $version";
+?>
+
+</body>
+</html>
+
