93f5f0577c64e1f1601d74fa74dbafc6ec900abf
Fouzi92 <fouzi457@hotmail.fr>
2023-07-06 00:34:05 +0200

    Formation developpeur

---
+++ Php/Les-variables-en-php/index-2-in-else.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+  
+/* $motivation =  7;
+
+if($motivation < 3 ) {
+    echo" Oulala bouge toi sale gland";
+}
+
+else if ( $motivation < 7 ) {
+
+    echo "courage arhy";
+ }
+  else 
+  {
+    echo"C'est bien arhy";
+  } */
+  // https://www.php.net/manual/fr/control-structures.switch.php méthode swich
+
+?>
+
+</body>
+</html>
+
